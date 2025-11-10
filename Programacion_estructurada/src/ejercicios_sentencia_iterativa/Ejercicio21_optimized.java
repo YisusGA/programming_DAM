f746aad0dc12d492022028689ff782932c2f05a4
Yisus <jga547@educa.madrid.org>
2025-11-10 10:37:40 +0100

    Changes in the src

--- Programacion_estructurada/src/ejercicios_sentencia_iterativa/Ejercicio21_optimized.java
+++ Programacion_estructurada/src/ejercicios_sentencia_iterativa/Ejercicio21_optimized.java
@@ -5,7 +5,8 @@ package ejercicios_sentencia_iterativa;
  * 
  * En esta versión: 
  * -Empezamos el contador en 3, pues no hay números primos divisibles entre 2 otros que el mismo 2 (Todos los numeros pares son divisibles entre 2, quitando la posibilidad de ser primos, entonces, si quitamos el propio 2 de la ecuacion, ganamos mucho tiempo).
- * -Fijamos el limite como Math.sqrt(n). Y vamos comprobando los divisores de 2 en 2. 3-5-7-9-11-13-15-17. Hasta llegar a la raiz cuadrada de 'n'. Si ninguno de los anteriores divisores, ni siquiera la propia raiz cuadrada de 'n', es divisor suyo, eso significa que es primo. 
+ * -Fijamos el limite como Math.sqrt(n). De esta forma, reducimos la complejidad de cálculo del algoritmo, pues ya no tiene que llegar comprobando números hasta el valor de operacionPrimo. Y vamos comprobando los divisores de 2 en 2. 3-5-7-9-11-13-15-17, 
+ *  hasta llegar a la raiz cuadrada de 'n'. Si ninguno de los anteriores divisores, ni siquiera la propia raiz cuadrada de 'n', es divisor suyo, eso significa que es primo. 
  *  Solo hemos comprobado los divisores IMPARES y hasta la raiz cuadrada del numero.
  */
 
@@ -14,21 +15,20 @@ import java.util.Scanner;
 public class Ejercicio21_optimized {
 
 	public static void main(String[] args) {
-		boolean esPrimo = true;
+		boolean esPrimo = true; //Es fundamental inicializar esto en true, pues lo que hacemos abajo es intentar demostrar si es false; si no se demuestra, entonces es true y el número es primo
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Introduce un número entero positivo");
 		int n = scan.nextInt();
-		if (n <= 0) {
-			System.err.println("Debes introducir un número entero positivo distinto de 0");
-			System.exit(0);
+		while (n <= 0) {
+			System.out.println("Debes introducir un número entero positivo distinto de 0");
+			n = scan.nextInt();
 		}
-		if (n < 2 || (n % 2 == 0 && n != 2)) {
+		//Si el número es par, excluyendo el 2, entonces no puede ser primo
+		if (n % 2 == 0 && n != 2) {
 			esPrimo = false;
-		}
-		else if (n == 2) {
-			esPrimo = true;
-		}
-		else {
+		//Aquí hay que poner un else if, porque si ponemos un else a secas, el 2 se consideraría no primo. Si el número es 2, como esPrimo se inicializa en true y no entra ni en el if
+		//ni en el else if, entonces esPrimo es true y el 2 se considera primo	
+		} else if (n > 2) {
 			int raiz = (int)Math.sqrt(n);
 			for (int i = 3; i <= raiz; i += 2) {
 				if (n % i == 0) {
--- Programacion_estructurada/src/ejercicios_sentencia_iterativa/Ejercicio22_numPerfectos_optimized.java
+++ Programacion_estructurada/src/ejercicios_sentencia_iterativa/Ejercicio22_numPerfectos_optimized.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
  *  fórmula:  numPerfecto = (2^(n - 1)) *  ((2^n) - 1). Lo que vamos a hacer es ir incrementando el valor de esa n desd 1 hasta el limit.
  *  En esta versión: 
  * -Empezamos el contador en 3, pues no hay números primos divisibles entre 2 otros que el mismo 2 (Todos los numeros pares son divisibles entre 2, quitando la posibilidad de ser primos, entonces, si quitamos el propio 2 de la ecuacion, ganamos mucho tiempo).
- * -Fijamos el limite como Math.sqrt(operacionPrimo). Y vamos comprobando los divisores de 2 en 2. 3-5-7-9-11-13-15-17. Hasta llegar a la raiz cuadrada de 'operacionPrimo'. Si ninguno de los anteriores divisores, ni siquiera la propia raiz cuadrada de 'operacionPrimo', es divisor suyo, eso significa que es primo. 
+ * -Fijamos el limite como Math.sqrt(operacionPrimo). De esta forma, reducimos la complejidad de cálculo del algoritmo, pues ya no tiene que llegar comprobando números hasta el valor de operacionPrimo. Y vamos comprobando los divisores de 2 en 2. 3-5-7-9-11-13-15-17, 
+ *  hasta llegar a la raiz cuadrada de 'operacionPrimo'. Si ninguno de los anteriores divisores, ni siquiera la propia raiz cuadrada de 'operacionPrimo', es divisor suyo, eso significa que es primo. 
  *  Solo hemos comprobado los divisores IMPARES y hasta la raiz cuadrada del numero.
  */
 
@@ -16,17 +17,15 @@ public class Ejercicio22_numPerfectos_optimized {
 
 	public static void main(String[] args) {
 		long limit = Integer.MAX_VALUE;
-		boolean primo = false;
 		for (int n = 1; n < limit; n++) {
 			double operacionPrimo = (Math.pow(2, n) - 1);
-			boolean esPrimo = true;
-			if (operacionPrimo < 2 || (operacionPrimo % 2 == 0 && operacionPrimo != 2)) {
+			boolean esPrimo = true; //Es fundamental inicializar esto en true, pues lo que hacemos abajo es intentar demostrar si es false; si no se demuestra, entonces es true y el número es primo
+			//Si el número es par, excluyendo el 2, entonces no puede ser primo
+			if (operacionPrimo % 2 == 0 && operacionPrimo != 2) {
 				esPrimo = false;
-			}
-			else if (operacionPrimo == 2) {
-				esPrimo = true;
-			}
-			else {
+			//Aquí hay que poner un else if, porque si ponemos un else a secas, el 2 se consideraría no primo. Si el número es 2, como esPrimo se inicializa en true y no entra ni en el if
+			//ni en el else if, entonces esPrimo es true y el 2 se considera primo
+			} else if (operacionPrimo > 2) {
 				int raiz = (int)Math.sqrt(operacionPrimo);
 				for (int i = 3; i <= raiz; i += 2) {
 					if (operacionPrimo % i == 0) {
@@ -34,7 +33,7 @@ public class Ejercicio22_numPerfectos_optimized {
 					}
 				}
 			}
-			if (primo) {
+			if (esPrimo) {
 				double numPerfecto = (Math.pow(2, (n -1)) * (Math.pow(2, n) - 1));
 				System.out.println("Número perfecto encontrado: " + numPerfecto);
 			}

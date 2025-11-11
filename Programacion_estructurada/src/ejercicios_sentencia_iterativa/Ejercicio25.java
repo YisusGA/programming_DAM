6bc32f76d899cd06e4cb4f82d4c58d903a576448
Yisus <jga547@educa.madrid.org>
2025-11-11 08:45:02 +0100

    updated code to apply fix

--- Programacion_estructurada/src/ejercicios_sentencia_iterativa/Ejercicio25.java
+++ Programacion_estructurada/src/ejercicios_sentencia_iterativa/Ejercicio25.java
@@ -12,6 +12,8 @@ public class Ejercicio25 {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Introduce un número entero positivo mayor que 0");
 		int n = scan.nextInt(); //Número sobre el que vamos a comprobar su triangularidad
+		int Ti = 1;
+		int i = 1;
 		boolean esTriangular = false; //Inicializamos el booleano en false, pues vamos a intentar demostrar si es true
 		while (n <= 0) { //Obligamos a que el número introducido sea natural (entero positivo)
 			System.err.println("Número no válido");
@@ -19,13 +21,14 @@ public class Ejercicio25 {
 			n = scan.nextInt();
 		}
 		//Calculamos todos los números triangulares desde el 1 hasta el número n, y los vamos comparando con n. Si n == Ti, entonces n es triangular
-		for (int i = 1; i <= n; i++) {
-			int Ti = i * (i + 1) / 2; //Fórmula para calcular los números triangulares
+		do {
+			Ti = i * (i + 1) / 2; //Fórmula para calcular los números triangulares
 			if (n == Ti) {
 				esTriangular = true;
 				break;
 			}
-		}
+			i++;
+		} while (Ti <= n);
 		if (esTriangular) {
 			System.out.println("El número introducido es triangular");
 		} else {

42dbfe94041ddc62f714bba0fa08e427288d3d3e
Yisus <jga547@educa.madrid.org>
2025-11-14 18:46:07 +0100

    minor format changes

--- Programacion_estructurada/src/arrays/Ejemplo01.java
+++ Programacion_estructurada/src/arrays/Ejemplo01.java
@@ -5,34 +5,35 @@ import java.util.Random;
 public class Ejemplo01 {
 
 	public static void main(String[] args) {
-		int [] numeros = new int[10];
-		numeros [0] = 3;
-		numeros [4] = 7;
+		int[] numeros = new int[10];
+		numeros[0] = 3;
+		numeros[4] = 7;
 		for (int i = 0; i < 10; i++) {
 			System.out.println("array: " + numeros[i]);
 		}
-		
+
 //		Si trato de acceder a una posición del array que no existe, mostrará un error de excepción: 
 //		java.lang.ArrayIndexOutOfBoundsException
 //		for (int i = 0; i <= 10; i++) {
 //			System.out.println(numeros[i]);
 //		}
-		
-		//Recorrer el array al revés
+
+		// Recorrer el array al revés
 		for (int i = 9; i >= 0; i--) {
 			System.out.println("array al revés: " + numeros[i]);
 		}
-		//Con esto, llenamos un array con números aleatorios
+		// Con esto, llenamos un array con números aleatorios
 		for (int i = 0; i < 10; i++) {
-			numeros [i] = (int)(Math.random() * 20);
+			numeros[i] = (int) (Math.random() * 20);
 			System.out.println("array aleatorio 1: " + numeros[i]);
 		}
-		//Con esto, llenamos un array con números aleatorios pero usando la clase Random
+		// Con esto, llenamos un array con números aleatorios pero usando la clase
+		// Random
 		Random generador = new Random();
 		for (int i = 0; i < 10; i++) {
-			numeros [i] = generador.nextInt();
+			numeros[i] = generador.nextInt();
 			System.out.println("array aleatorio 2: " + numeros[i]);
-		}	
+		}
 	}
 
 }
--- Programacion_estructurada/src/arrays/Ejercicio01.java
+++ Programacion_estructurada/src/arrays/Ejercicio01.java
@@ -9,16 +9,17 @@ import java.util.Scanner;
 public class Ejercicio01 {
 
 	public static void main(String[] args) {
-		char [] letras;
+		char[] letras;
 		int counter = 0;
-		letras = new char [5];
+		letras = new char[5];
 		Scanner scan = new Scanner(System.in);
 		String linea = "";
 		for (int i = 0; i < 5; i++) {
 			System.out.println("Introduce un nombre");
-			String nombre = scan.nextLine();;
-			letras [i] = nombre.charAt(0); 
-			if (letras [i] >= 65 && letras [i] <= 90) {
+			String nombre = scan.nextLine();
+			;
+			letras[i] = nombre.charAt(0);
+			if (letras[i] >= 65 && letras[i] <= 90) {
 				counter++;
 				linea += nombre;
 			}

205b11cf4df53b2b6494277a742854b21da6bd1b
Yisus <jga547@educa.madrid.org>
2025-11-15 11:15:58 +0100

    new exercises

---
+++ Programacion_estructurada/src/ejercicios_arrays/Ejercicio5.java
@@ -0,0 +1,25 @@
+package ejercicios_arrays;
+
+/**
+ * Leer 10 números enteros. Debemos mostrarlos en el siguiente orden: el primero, el último, el segundo, el penúltimo, el tercero, etc.
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] numeros = new int[10];
+		for (int i = 0; i < numeros.length; i++) {
+			System.out.println("Introduce un número entero");
+			numeros[i] = scan.nextInt();
+		}
+		System.out.println("La secuencia solicitada de números es: ");
+		for (int i = 0; i < (numeros.length / 2); i++) {
+			System.out.printf("%1d %1d ", numeros[i], (numeros.length - 1 - i));
+		}
+		scan.close();
+	}
+
+}
---
+++ Programacion_estructurada/src/ejercicios_sentencia_iterativa/Ejercicio27.java
@@ -0,0 +1,106 @@
+package ejercicios_sentencia_iterativa;
+
+/**
+ * Programa que dado un número entero lo exprese en el sistema de numeración romano
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio27 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String unidades = "";
+		String decenas = "";
+		String centenas = "";
+		String miles = "";
+		System.out.println("Introduce un número entero comprendido entre 0 y 3999");
+		int num = scan.nextInt();
+		while (num < 0 || num > 3999) {
+			System.out.println("Número no válido, prueba de nuevo");
+			num = scan.nextInt();
+		}
+		int numOriginal = num;
+		scan.close();
+		int[] cifras = new int[("" + num).length()]; //Sacamos el número de cifras del número y asignamos ese tamaño al array
+		for (int i = 0; num > 0; i++) {
+			cifras[i] = num % 10;
+			num /= 10;
+		}
+		//UNIDADES
+		switch (cifras[0]) {
+			case 1, 2, 3:
+				for (int i = 1; i <= cifras[0]; i++) {
+					unidades += "I";
+				}
+			break;
+			case 4:
+				unidades = "IV";
+				break;
+			case 5:
+				unidades = "V";
+				break;
+			case 6, 7, 8:
+				unidades = "V";
+			for (int i = 6; i <= cifras[0]; i++) {
+				unidades += "I";
+			}
+			break;
+			case 9:
+				unidades = "IX";
+				break;
+			}
+		//DECENAS
+		switch (cifras[1]) {
+		case 1, 2, 3:
+			for (int i = 1; i <= cifras[1]; i++) {
+				decenas += "X";
+			}
+		break;
+		case 4:
+			decenas = "XL";
+			break;
+		case 5:
+			decenas = "L";
+			break;
+		case 6, 7, 8:
+			decenas = "L";
+		for (int i = 6; i <= cifras[1]; i++) {
+			decenas += "X";
+		}
+		break;
+		case 9:
+			decenas = "XC";
+			break;
+		}
+		//CENTENAS
+		switch (cifras[2]) {
+		case 1, 2, 3:
+			for (int i = 1; i <= cifras[2]; i++) {
+				centenas += "C";
+			}
+		break;
+		case 4:
+			centenas = "CD";
+			break;
+		case 5:
+			centenas = "D";
+			break;
+		case 6, 7, 8:
+			centenas = "D";
+		for (int i = 6; i <= cifras[2]; i++) {
+			centenas += "C";
+		}
+		break;
+		case 9:
+			centenas = "CM";
+			break;
+		}
+		//MILES
+		for (int i = 1; i <= cifras[3]; i++) {
+			miles += "M";
+		}
+		System.out.println("El número romano de " + numOriginal + " es: " + miles + centenas + decenas + unidades);
+	}
+
+}

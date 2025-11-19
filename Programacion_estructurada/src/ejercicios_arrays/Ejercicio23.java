f671d0bb6401b10a33b8948822fb1fdea6c9d286
Yisus <jga547@educa.madrid.org>
2025-11-19 18:44:57 +0100

    corregido y funcional

--- Programacion_estructurada/src/ejercicios_arrays/Ejercicio23.java
+++ Programacion_estructurada/src/ejercicios_arrays/Ejercicio23.java
@@ -14,45 +14,50 @@ public class Ejercicio23 {
 		System.out.println("Introduce el tamaño deseado de array");
 		int arraySize = scan.nextInt();
 
-		int[] array1 = new int[arraySize];
-		int[] array2 = new int[arraySize];
+		int[] arrayOriginal = new int[arraySize];
+		int[] arrayOrdenado = new int[arraySize];
 
 		for (int i = 0; i < arraySize; i++) {
 			System.out.println("Introduce un número entero");
-			array1[i] = scan.nextInt();
+			arrayOriginal[i] = scan.nextInt();
 		}
 		System.out.print("Array original: ");
-		for (int i : array1) {
+		for (int i : arrayOriginal) {
 			System.out.print(i + ", ");
 		}
-		int min = Integer.MAX_VALUE;
 //		for (int i = 0; i < arraySize; i++) {
 //			for (int k = 0; k < arraySize; k++) {
-//				if (array1[k] < min) {
-//					min = array1[k];
-//					array2[i] = min;
+//				if (arrayOriginal[k] < min) {
+//					min = arrayOriginal[k];
+//					arrayOrdenado[i] = min;
 //				}
 //			}
 //		}
-		for (int i = 0; i < arraySize; i++) {
-			for (int j = 1; j < arraySize; j++) {
-				if (array1[j] < array1[j - 1]) {
-					min = array1[j];
+		for (int i = 0; i <arraySize; i++) {
+			//Encontramos el mínimo del arrayOriginal
+			int min = arrayOriginal[0];
+			for (int j = 1; j < arrayOriginal.length; j++) {
+				if (arrayOriginal[j] < min) {
+					min = arrayOriginal[j];
 				}
-				array2[i] = min;
-				System.out.println("array 2 : " + array2[i]);
-				System.out.println("min: " + min);
-				for (int k = 0; k < arraySize; k++) {
-					if (array1[k] == min) {
-						array1[k] = Integer.MAX_VALUE;
-						System.out.println("array 1: " + array1[k]);
-					}
+			}	
+			
+			//Llenamos la posición que toque del arrayOrdenado con el mínimo encontrado
+			arrayOrdenado[i] = min;
+			
+			 
+			for (int k = 0; k < arrayOriginal.length; k++) {
+				//Iteramos en el arrayOriginal hasta encontrar la posición que coincide con el min encontrado, y le asignamos el valor más alto posible, un Integer.MAX_VALUE, para que jamás vuelva a encontrarse como mínimo
+				if (arrayOriginal[k] == min) {
+					arrayOriginal[k] = Integer.MAX_VALUE;
+					break; //Metiendo este break de aquí, lo que logramos es que en cuanto encuentre una coincidencia, se salga del bucle for en el que está este bloque de código. Si no hacemos esto, si hay varios números
+						   //iguales en el arrayOriginal, al encontrarse con los sucesivos, los va a rellenar con un Integer.MAX_VALUE y los perderemos
 				}
 			}
-		}
+		}	
 		System.out.println();
 		System.out.print("Nuevo array ordenado: ");
-		for (int i : array2) {
+		for (int i : arrayOrdenado) {
 			System.out.print(i + ", ");
 		}
 		scan.close();

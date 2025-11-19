a874a9212dfaa712e493ae1d10044d93d7d5d4d3
Yisus <jga547@educa.madrid.org>
2025-11-19 18:45:45 +0100

    fallo encontrado y añadida explicación

--- Programacion_estructurada/src/ejercicios_arrays/Ejercicio23_profe.java
+++ Programacion_estructurada/src/ejercicios_arrays/Ejercicio23_profe.java
@@ -1,50 +1,70 @@
 package ejercicios_arrays;
 
+/**
+ * Dado un array de n enteros pasados por teclado, ordenarlo (el resultado será un nuevo array)
+ */
+
 import java.util.Scanner;
 
 public class Ejercicio23_profe {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-
+		System.err.println("Esta versión que hizo la profe tiene el problema de que si arrayOriginal tiene 2 números iguales, mete un 0. Ver Ejercicio23_profe_Gemini para ver la solución");
+		
+		//Leemos el tamaño del array
 		System.out.println("Introduce el tamaño deseado de array");
 		int arraySize = scan.nextInt();
 
-		int[] array1 = new int[arraySize];
-		int[] array2 = new int[arraySize];
+		//Inicializamos los arrays
+		int[] arrayOriginal = new int[arraySize];
+		int[] arrayOrdenado = new int[arraySize];
 
+		//Llenamos el arrayOriginal
 		for (int i = 0; i < arraySize; i++) {
 			System.out.println("Introduce un número entero");
-			array1[i] = scan.nextInt();
+			arrayOriginal[i] = scan.nextInt();
 		}
+		
+		//Imprimimos el arrayOriginal para ver cómo es
 		System.out.print("Array original: ");
-		for (int i : array1) {
+		for (int i : arrayOriginal) {
 			System.out.print(i + ", ");
 		}
 		for (int i = 0; i <arraySize; i++) {
-			//encontrar mínimo
-			int min = array1[0];
-			for (int j = 1; j < array1.length; j++) {
-				System.out.println("j: " + j);
-				if (array1[j] < min) {
-					min = array1[j];
+			//Encontramos el mínimo del arrayOriginal
+			int min = arrayOriginal[0];
+			for (int j = 1; j < arrayOriginal.length; j++) {
+				if (arrayOriginal[j] < min) {
+					min = arrayOriginal[j];
 				}
 			}
-			//Eliminar min del array1. Hacemos lo que vimos en el Ejercicio18_profe para reducir el tamaño de un array eliminando una posición concreta,
-			//que en este caso es el mínimo que ya hemos sacado
-			int[] aux = new int[array1.length -1];
-			for (int j = 0, t = 0; j < array1.length; j++) {
-				if (array1[j] != min) {
-					aux[t++] = array1 [j];
-				}
-			}
-			array1 = aux;
 			
-			array2[i] = min;
+			//Llenamos la posición que toque del arrayOrdenado con el mínimo encontrado
+			arrayOrdenado[i] = min;
+			
+			//Eliminar min del arrayOriginal. Hacemos lo que vimos en el Ejercicio18_profe para reducir el tamaño de un array eliminando una posición concreta,
+			//que en este caso es el mínimo que ya hemos sacado. Importante usar arrayOriginal.length y no arraySize, pues el tamaño del arrayOriginal se va a ir decrementando en cada iteración
+			int[] aux = new int[arrayOriginal.length -1];
+			for (int j = 0, t = 0; j < arrayOriginal.length; j++) {
+				//Esta versión que hizo la profe tiene el problema de que si arrayOriginal tiene 2 números iguales, mete un 0 por cada número repetido. Esto se debe a que los números repetidos 
+				//coinciden con el min, y entonces no entra en el bucle para todos esos números. Pero como el arrayOriginal sólo se reduce en una posición, entonces se rellena el hueco con un 0.
+				//Ver Ejercicio23_profe_Gemini para ver la solución propuesta por Gemini
+				if (arrayOriginal[j] != min) {
+					aux[t++] = arrayOriginal [j];
+				}
+			}
+			//Asignamos al arrayOriginal la dirección de memoria del array aux, con lo cual, ahora arrayOriginal apunta a un array que tiene un tamaño decrementado en 1 y que tiene todos los valores
+			//que tenía originalmente arrayOriginal exceptuando el min que encontramos
+			arrayOriginal = aux;
+			
+			
 		}
+		
+		//Imprimimos el nuevo arrayOrdenado, ya ordenado
 		System.out.println();
 		System.out.print("Nuevo array ordenado: ");
-		for (int i : array2) {
+		for (int i : arrayOrdenado) {
 			System.out.print(i + ", ");
 		}
 		scan.close();

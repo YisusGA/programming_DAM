3661e1629ae41067e964f3284aa4524ef71c5a29
Yisus <jga547@educa.madrid.org>
2025-11-24 19:52:21 +0100

    corregida tabulación

--- Programacion_estructurada/src/ejercicios_arrays/Ejercicio15.java
+++ Programacion_estructurada/src/ejercicios_arrays/Ejercicio15.java
@@ -12,9 +12,9 @@ public class Ejercicio15 {
 		Scanner scan = new Scanner(System.in);
 		int[] numeros = new int[10];
 		for (int i = 0; i < 10; i++) {
-		System.out.println("Introduce un número entero");
-		numeros[i] = scan.nextInt();
-		}	
+			System.out.println("Introduce un número entero");
+			numeros[i] = scan.nextInt();
+		}
 		System.out.print("Array original: ");
 		for (int i = 0; i < 10; i++) {
 			System.out.print(numeros[i] + " ");

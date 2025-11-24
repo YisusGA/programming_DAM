1e925f92a552a9c157c50dca8d485d9f17ac44dc
Yisus <jga547@educa.madrid.org>
2025-11-24 17:49:41 +0100

    nuevos ejercicios tipo examen. El 2 no está completo aún

---
+++ Programacion_estructurada/src/examen_año_anterior03/Ejercicio01.java
@@ -0,0 +1,69 @@
+package examen_año_anterior03;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		boolean cuenta = false;
+		int contadorPlato1 = 0;
+		int contadorPlato2 = 0;
+		int contadorPostre = 0;
+		double precioPrimero = 8;
+		double precioSegundo = 10;
+		double preciocontadorPostre = 5;
+		double precioMenu = 20;
+		int contadorMenu = 0;
+		while(!cuenta) {
+			int opcion;
+			System.out.println("""
+					Bienvenido al Restaurante de Yisus
+					Elige una opción:
+					1. Ordenar un primer plato que vale 8 euros
+					2. Ordenadr un segundo plato que vale 10 euros
+					3. Ordenar un contadorPostre que vale 5 euros
+					4. Pedir cuenta
+					""");
+			opcion = scan.nextInt();
+			switch (opcion) {
+			case 1:
+				contadorPlato1++;
+				break;
+			case 2:
+				contadorPlato2++;
+				break;
+			case 3:
+				contadorPostre++;
+				break;
+			case 4:	
+				cuenta = true;
+				break;
+			default:
+				System.out.println("El número introducido no es válido, introduce un número entre 1 y 4");
+				break;
+			}
+		}
+		while (contadorPlato1 > 0 && contadorPlato2 > 0 && contadorPostre > 0) {
+				contadorMenu++;
+				contadorPlato1--;
+				contadorPlato2--;
+				contadorPostre--;
+		}
+		double precio = precioMenu * contadorMenu + precioPrimero * contadorPlato1 + precioSegundo * contadorPlato2 + preciocontadorPostre * contadorPostre;
+		if (precio >= 51) {
+			precio *= 0.9; 
+		}
+		System.out.printf(" Has pedido: %n %d menus %n %d primeros platos %n %d segundos platos %n %d postres. %n Te has gastado: %.2f", contadorMenu, contadorPlato1, contadorPlato2, contadorPostre, precio);
+//		System.out.printf("""
+//							 Has pedido: 
+//							 %d menus
+//							 %d primeros platos
+//							 %d segundos platos
+//							 %d postres
+//							 Te has gastado: %.2f""", 
+//							 contadorMenu, contadorPlato1, contadorPlato2, contadorPostre, precio);
+//		scan.close();
+	}
+
+}
---
+++ Programacion_estructurada/src/examen_año_anterior03/Ejercicio02.java
@@ -0,0 +1,48 @@
+package examen_año_anterior03;
+
+import java.util.Scanner;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = 1;
+		int arraySize = 5;
+		int[] numeros = new int[arraySize];
+		boolean igual = false;
+		int c = 0;
+		do {
+			igual = false;
+			System.out.println("Introduce un número");
+			num = scan.nextInt();
+			for (int j = 0; j < numeros.length; j++) {
+				if (numeros[j] == num) {
+					igual = true;
+				}
+			}
+			if (!igual && num > 0) {
+				numeros[c] = num;
+				c++;
+			}
+		} while (c < 5);
+		System.out.print("Array introducido: ");
+		for (int i : numeros) {
+			System.out.print(i + ", ");
+		}
+		igual = false;
+		System.out.println();
+		while (!igual) {
+			System.out.println("Introduce el número a comprobar");
+			int numComp = scan.nextInt();
+			for (int i = 0; i < numeros.length; i++) {
+				if (numeros[i] == numComp) {
+					igual = true;
+				}
+			}
+		}
+		if (igual) {
+			System.out.println("El programa ha finalizado");
+		}
+	}
+
+}

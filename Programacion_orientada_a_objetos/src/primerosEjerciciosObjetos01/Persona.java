a0eea66856a65cf7c44a3bedd32b6f79b45b21c8
Yisus <jga547@educa.madrid.org>
2025-12-02 17:20:06 +0100

    ejercicios completados

--- Programacion_orientada_a_objetos/src/primerosEjerciciosObjetos01/Persona.java
+++ Programacion_orientada_a_objetos/src/primerosEjerciciosObjetos01/Persona.java
@@ -1,13 +1,13 @@
-package primerosEjerciciosObjetos;
+package primerosEjerciciosObjetos01;
 
 class Persona {
-	String nombre;
-	int edad;
-	void saludar() {
-		System.out.println("Hola");
+	String nombre; //Nombre de la persona
+	int edad; //Edad de la persona
+	void saludar() { //Método para saludar
+		System.out.printf("Hola soy %s y tengo %d años %n", nombre, edad);
 	}
 	void despedirse() {
-		System.out.println("Adios");
+		System.out.println("Adios"); //Método para despedirse
 	}
 }
 
--- Programacion_orientada_a_objetos/src/primerosEjerciciosObjetos01/Prueba.java
+++ Programacion_orientada_a_objetos/src/primerosEjerciciosObjetos01/Prueba.java
@@ -1,4 +1,4 @@
-package primerosEjerciciosObjetos;
+package primerosEjerciciosObjetos01;
 
 public class Prueba {
 
@@ -6,6 +6,40 @@ public class Prueba {
 		Persona pepe = new Persona();
 		pepe.saludar();
 		pepe.despedirse();
+		System.out.println(pepe.edad);
+		System.out.println(pepe.nombre);
+		pepe.edad = 20;
+		pepe.nombre = "Pepe";
+		System.out.println(pepe.edad);
+		System.out.println(pepe.nombre);
+		pepe.saludar();
+		pepe.despedirse();
+		
+		Persona ana = new Persona();
+		ana.nombre = "Ana";
+		ana.edad = 18;
+		
+		pepe.saludar();
+		ana.saludar();
+		
+		
+		/*
+		 * Con esto de debajo, crearíamos una variable tipo objeto con el nombre “aux”, que tomaría la dirección de memoria de
+		 * la variable tipo objeto “pepe”. Por tanto, al operar con esa variable aux, tendrá los mismos valores en sus propiedades 
+		 * que la variable pepe. Y por tanto, al usar el método saludar() con ella, mostraría el nombre y la edad almacenados 
+		 * en las propiedades de la instancia pepe. En este punto del programa, tendríamos 3 objetos (o instancias) de la clase 
+		 * Persona: pepe, ana y aux.
+		 */
+		Persona aux = pepe;
+		aux.saludar();
+		
+		/*
+		 * Con esto de debajo, asignamos a la instancia "ana" la dirección de memoria de aux, que ya tenía, por la asignación previa, 
+		 * la dirección de memoria de la instancia "pepe". Por tanto, ahora las 3 instancias apuntan a la misma dirección de memoria,
+		 * y tendrán los mismos valores para sus propiedades
+		 */
+		ana = aux;
+		ana.saludar();
 	}
 
 }

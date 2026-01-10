0bf6e8142d9dce06d22133ca924eec9fc2ae9e28
Yisus <jga547@educa.madrid.org>
2026-01-10 10:21:53 +0100

    ampliados los ejemplos

--- Prueba_import_JAR/src/main/Main.java
+++ Prueba_import_JAR/src/main/Main.java
@@ -7,9 +7,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//Leer entero
 		System.out.println("Introduce un número entero");
-		int num = Teclado2.leerEntero();
-		System.out.println("El número introducido es: " + num);
+		int numInt = Teclado2.leerEntero();
+		System.out.println("El número introducido es: " + numInt);
+		
+		//Leer decimal
+		System.out.println("Introduce un número decimal");
+		double numDouble = Teclado2.leerDecimal();
+		System.out.println("El número introducido es: " + numDouble);
+		
+		//Leer cadena
+		System.out.println("Introduce una palabra o frase");
+		String cadena = Teclado2.leerCadena();
+		System.out.println("La frase introducida es: " + cadena);
 	}
 
 }

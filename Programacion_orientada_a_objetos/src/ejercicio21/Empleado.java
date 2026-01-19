e3e04c1dbf4a2bb55f9b9c50ff612cf5b022dce8
Yisus <jga547@educa.madrid.org>
2026-01-19 19:43:45 +0100

    mejor formateados

--- Programacion_orientada_a_objetos/src/ejercicio21/Empleado.java
+++ Programacion_orientada_a_objetos/src/ejercicio21/Empleado.java
@@ -3,44 +3,50 @@ package ejercicio21;
 import java.util.Date;
 
 public class Empleado {
-	
+
 	private String nif, nombre;
 	private double sueldo;
 	private Date fechaContrato;
 	private Departamento departamento;
-	
-	
+
 	public String getNif() {
 		return nif;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public double getSueldo() {
 		return sueldo;
 	}
+
 	public void setNif(String nif) {
 		this.nif = nif;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public void setSueldo(double sueldo) {
 		this.sueldo = sueldo;
 	}
+
 	public Departamento getDepto() {
 		return departamento;
 	}
+
 	public void setDepto(Departamento departamento) {
 		this.departamento = departamento;
 	}
+
 	public Date getFechaContrato() {
 		return fechaContrato;
 	}
+
 	public void setFechaContrato(Date fechaContrato) {
 		this.fechaContrato = fechaContrato;
 	}
-	
-	
 
 }

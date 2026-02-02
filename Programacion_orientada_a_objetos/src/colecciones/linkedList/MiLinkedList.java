93f021c25b4d08b9cadd555fae255d4cbfa16a50
Yisus <jga547@educa.madrid.org>
2026-02-02 20:50:54 +0100

    completadas algunas cosas

--- Programacion_orientada_a_objetos/src/colecciones/linkedList/MiLinkedList.java
+++ Programacion_orientada_a_objetos/src/colecciones/linkedList/MiLinkedList.java
@@ -2,11 +2,29 @@ package colecciones.linkedList;
 
 public class MiLinkedList<T> {
 	private Nodo<T> inicio;
-	
-	public void addFirst (T dato) {
+
+	public void addFirst(T dato) { // Método para añadir un nuevo nodo al inicio de la lista. Fijarse en que usamos
+									// el constructor de Nodo para crear ese nuevo nodo, con el parámetro T dato
+									// pasado entre paréntesis
 		Nodo<T> nodo = new Nodo<>(dato);
-		nodo.ste = inicio;
+		nodo.setSiguiente(inicio);
 		inicio = nodo;
+	}
+
+	public int size() { // No sé si esto está bien
+		// ToDo
+		int size = 0;
+		Nodo<T> nodo = new Nodo<>();
+		nodo.setSiguiente(inicio);
+		do {
+			nodo.setSiguiente(nodo);
+			size++;
+		} while (nodo.getSiguiente() != null);
+		return size;
+	}
+
+	public Nodo<T> get() {
+		// ToDo
 	}
 
 }
--- Programacion_orientada_a_objetos/src/colecciones/linkedList/Nodo.java
+++ Programacion_orientada_a_objetos/src/colecciones/linkedList/Nodo.java
@@ -1,13 +1,27 @@
 package colecciones.linkedList;
 
 public class Nodo<T> {
-	
+
 	private T dato;
-	private Nodo<T> ste; //ste = siguiente
-	
-	Nodo(T dato) {
+	private Nodo<T> siguiente;
+
+	Nodo(T dato) { // Esto es el método constructor del nodo
 		this.dato = dato;
-		ste = null;
+		siguiente = null;
+	}
+	
+	Nodo() { // Esto es el método constructor del nodo
+		this.dato = dato;
+		siguiente = null;
+	}
+	
+	public Nodo<T> getSiguiente() {
+		return this.siguiente;
+	}
+
+	//Método para cambiar a dónde apunta el nuevo nodo que se inserte en la LinkedList
+	public void setSiguiente(Nodo<T> siguiente) {
+		this.siguiente = siguiente;
 	}
 
 }

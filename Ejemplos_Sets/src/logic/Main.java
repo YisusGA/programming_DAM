dc22f38513e842aba20e5bdc88b507a19007ac08
Yisus <jga547@educa.madrid.org>
2026-02-11 19:16:44 +0100

    mejorados los comentarios y puesto todo más bonico

--- Ejemplos_Sets/src/logic/Main.java
+++ Ejemplos_Sets/src/logic/Main.java
@@ -10,10 +10,20 @@ public class Main {
 		// Set es la interfaz, y hay que implementarla.Hay 3 implementaciones de sets
 		// (conjuntos): HashSet, LinkedHashSet, TreeSet
 
-		// ejemploHashSet();
+		System.out.println("HashSet de Strings");
+		System.out.println("------------------");
+		System.out.println();
+		ejemploHashSet();
+		System.out.println();
+		System.out.println();
+		System.out.println("HashSet de Alumno");
+		System.out.println("-----------------");
+		System.out.println();
 		ejemploAlumnoHashSet();
 
 	}
+
+	// MÉTODOS AUXILIARES PARA PROBAR COMO SE TRABAJA CON HASHSET
 
 	private static void ejemploHashSet() {
 		Set<String> nombres = new HashSet<>(); // Implementamos este set como HashSet, con objetos String
@@ -21,16 +31,25 @@ public class Main {
 		nombres.add("Juan");
 		nombres.add("Ana");
 		nombres.add("Bea");
+		System.out.println("Resultado tras añadir elementos");
 		System.out.println("Tamaño: " + nombres.size());
 		System.out.println();
 
-		nombres.add("Juan"); // Este no lo añadirá, porque ya existe uno igual y no puede haber duplicados
+		for (String n : nombres) {
+			System.out.println(n);
+		}
+		System.out.println();
+
+		nombres.add("Juan"); // Este no lo añadirá, porque ya existe uno igual y no puede haber duplicados en
+								// un set, dado que los elementos no tienen una posición y por tanto no se
+								// podrían distinguir entre sí
 		System.out.println("Tamaño tras intentar añadir elemento duplicado: " + nombres.size()); // Y el tamaño sigue
 																									// siendo el mismo
 		System.out.println();
 
 		// Recorrer colección. El orden de iteración no es el de inserción (no hay un
 		// orden como en las listas, pues esto es un conjunto)
+		System.out.println("Resultado tras intentar añadir elemento duplicado");
 		for (String n : nombres) {
 			System.out.println(n);
 		}
@@ -46,6 +65,7 @@ public class Main {
 		Iterator<String> it = nombres.iterator(); // Iterador se convierte en un objeto puntero que puede ir apuntando a
 													// los diferentes elementos de la colección. Comienza apuntando a la
 													// posición anterior a donde hay elemento
+		System.out.println("Imprimiendo con un iterator");
 		while (it.hasNext()) { // Mientras haya un elemento siguiente
 			String n = it.next(); // Cargamos el iterador con el elemento siguiente, y lo almacenamos en una
 									// variable
@@ -53,24 +73,33 @@ public class Main {
 		}
 		System.out.println();
 
-		// El iterator es poco útil porque se puede hacer un for-each. Sin embargo, sí
-		// que tiene la utilidad de que implementa un método remove
+		// El iterator es poco útil para hacer algo como lo de encima de estas líneas
+		// (recorrer e imprimir), porque esto se puede hacer de forma mucho más sencilla
+		// con un for-each. Sin embargo, el iterator tiene la gran utilidad de que
+		// implementa un método remove, que es algo que no puede usarse con un for-each
+		// debido a los problemas de concurrencia que genera
 
 		// Eliminar los nombres que empiecen por "A"
 
-		// Esto me generaría problemas de concurrencia
+		// 1- Esto me generaría problemas de concurrencia
 //		for (String n : nombres) {
 //			if (n.charAt(0) == 'A') {
 //				nombres.remove(n);
 //			}
 //		}
 
+		// 2- Usando el método remove de Iterator
+		System.out.println("Eliminando un String que empiece por A con el método remove() de iterator");
 		it = nombres.iterator();
 		while (it.hasNext()) {
 			String n = it.next();
 			if (n.charAt(0) == 'A') {
 				it.remove();
 			}
+			// También se podría hacer así
+//			if (it.next().charAt(0) == 'A') {
+//				it.remove();
+//			}
 		}
 		for (String n : nombres) {
 			System.out.println(n);
@@ -102,11 +131,13 @@ public class Main {
 			System.out.println(a);
 		}
 		System.out.println();
-		
+
+		// Para todo lo que viene debajo (contains, remove), sería necesario haber
+		// implementado un hashCode() y equals() en nuestra clase Alumno
 		System.out.println(alumnos.contains(new Alumno("5678", 8.0)));
-		
+
 		alumnos.remove(new Alumno("5678", 8.0));
-		
+
 		// BORRAR SUSPENSOS
 		Iterator<Alumno> it = alumnos.iterator();
 		while (it.hasNext()) {

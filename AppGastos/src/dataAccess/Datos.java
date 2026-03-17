a8d08bfeea9263cca939d93dab272610b53918e1
Yisus <jga547@educa.madrid.org>
2026-03-17 14:10:02 +0100

    añadidas mas funcionalidades a la app

--- AppGastos/src/dataAccess/Datos.java
+++ AppGastos/src/dataAccess/Datos.java
@@ -58,10 +58,11 @@ public class Datos {
 
 			File temp = new File("datos\\temp"); // Se crea un objeto File llamado temp, y se asocia con el fichero
 													// físico datos\\temp que creamos y rellenamos antes
-			
+
 			file.delete(); // Aquí se borra el fichero físico asociado al objeto file, pero el objeto file
 							// sigue existiendo en memoria
-			temp.renameTo(file); // Y se renombra el fichero datos\\temp con el nombre que está almacenado en el objeto file, que es "datos\\mes.csv"
+			temp.renameTo(file); // Y se renombra el fichero datos\\temp con el nombre que está almacenado en el
+									// objeto file, que es "datos\\mes.csv"
 
 		}
 	}
@@ -72,7 +73,7 @@ public class Datos {
 		if (file.exists()) {
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line;
-			while((line = br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				gastos += Double.parseDouble(line.split(";")[1]);
 			}
 			br.close();
@@ -83,19 +84,121 @@ public class Datos {
 	public static int diaMayorGasto(String mes) throws IOException {
 		int dia = 0;
 		File file = new File("datos\\" + mes + ".csv");
-		if(file.exists()) {
+		if (file.exists()) {
 			double mayorGasto = 0;
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String line;
-			while((line = br.readLine()) != null) {
-				if(Double.parseDouble(line.split(";")[1]) > mayorGasto) {
-					mayorGasto = Double.parseDouble(line.split(";")[1]); 
+			while ((line = br.readLine()) != null) {
+				if (Double.parseDouble(line.split(";")[1]) > mayorGasto) {
+					mayorGasto = Double.parseDouble(line.split(";")[1]);
 					dia = Integer.parseInt(line.split(";")[0]);
 				}
 			}
 			br.close();
 		}
 		return dia;
+	}
+
+	/**
+	 * 
+	 * @param mes del que se quiere eliminar el día
+	 * @param dia a eliminar
+	 * @return true si se ha eliminado o false si no
+	 * @throws IOException
+	 */
+	public static boolean eliminarDia(String mes, int dia) throws IOException {
+		boolean result = false;
+		File file = new File("datos\\" + mes + ".csv");
+		if (file.exists()) {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = br.readLine()) != null && !result) {
+				if (line.split(";")[0].equals(dia + "")) {
+					result = true;
+				}
+			}
+			br.close();
+		}
+		if (result) {
+			FileWriter fw = new FileWriter("datos\\temp");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (!line.split(";")[0].equals(dia + "")) {
+					fw.write(line + "\n");
+				}
+			}
+			br.close();
+			fw.close();
+
+			File temp = new File("datos\\temp");
+			file.delete();
+			temp.renameTo(file);
+		}
+		return result;
+	}
+
+	/**
+	 * Devuelve el mes con mayor gasto y el gasto de ese mes
+	 * 
+	 * @return El mes con mayor gasto y el gasto de ese mes. Si no existe el
+	 *         directorio o si no hay ficheros dentro del directorio, devuelve null
+	 * @throws IOException
+	 */
+	public static String mesMayorGasto() throws IOException {
+		int mayorGasto = 0;
+		String mesMayorGasto = null;
+		String result = null;
+		File directorio = new File("datos"); // Un objeto File no tiene por qué asociarse a un fichero, puede asociarse
+												// a un directorio
+		if (directorio.exists()) {
+			File[] archivos = directorio.listFiles(); // El método listFiles() de File nos permite listar los archivos
+														// (ficheros y directorios) dentro del archivo asociado al
+														// objeto File. Lo que devuelve es un array de objetos File
+			if (archivos.length > 0) {
+				for (File i : archivos) {
+					int gastoMes = 0;
+					String mes = "";
+					if (i.isFile()) {
+						BufferedReader br = new BufferedReader(new FileReader(i));
+						String line = br.readLine();
+						gastoMes += Double.parseDouble(line.split(";")[1]);
+						mes = i.getName();
+					}
+					if (gastoMes > mayorGasto) {
+						mayorGasto = gastoMes;
+						mesMayorGasto = mes;
+					}
+				}
+				if (mayorGasto != 0) {
+					result = "El mes con mayor gasto ha sido " + mesMayorGasto + " y se gastó " + mayorGasto;
+				}
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Devuelve un String con los gastos de un mes, o null si no existe el fichero
+	 * para ese mes
+	 * 
+	 * @param mes del que se quiere hacer el listado
+	 * @return Devuelve un String con los gastos de un mes, o null si no existe el
+	 *         fichero para ese mes
+	 * @throws IOException
+	 */
+	public static String listadoGastosMes(String mes) throws IOException {
+		String lista = null;
+		File file = new File("datos\\" + mes + ".csv");
+		if (file.exists()) {
+			lista = "";
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line;
+			while ((line = br.readLine()) != null) {
+				lista += line.split(";")[1] + " €, ";
+			}
+		}
+		return lista;
 	}
 
 }
--- AppGastos/src/main/Main.java
+++ AppGastos/src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import dataAccess.Datos;
@@ -15,10 +16,13 @@ public class Main {
 					1. Añadir gasto para día y mes
 					2. Ver gastos de un mes
 					3. Día de más gasto en un mes
+					4. Eliminar día
+					5. Mes con mayor gasto
+					6. Listar gastos de un mes
 					0. Salir
 					""");
 			op = Integer.parseInt(scan.nextLine()); // parseInt para que no nos den problemas los saltos de línea
-			switch(op) {
+			switch (op) {
 			case 1 -> {
 				System.out.println("Mes: ");
 				String mes = scan.nextLine();
@@ -33,7 +37,7 @@ public class Main {
 					System.err.println("No pudo añadirse el gasto");
 					e.printStackTrace();
 				}
-				
+
 			}
 			case 2 -> {
 				System.out.println("Mes a consultar");
@@ -55,6 +59,58 @@ public class Main {
 					e.printStackTrace();
 				}
 			}
+			case 4 -> {
+				System.out.println("Mes a consultar");
+				String mes = scan.nextLine();
+				boolean diaValido = false;
+				int dia = 0;
+				while (!diaValido) {
+					try {
+						System.out.println("Día a eliminar");
+						dia = scan.nextInt();
+						diaValido = true;
+						scan.nextLine();
+					} catch (InputMismatchException e) {
+						System.err.println("Día no válido, prueba de nuevo");
+						e.printStackTrace();
+					}
+				}
+				boolean eliminado = false;
+				try {
+					eliminado = Datos.eliminarDia(mes, dia);
+				} catch (IOException e) {
+					System.err.println("No se pudo eliminar ese día");
+					e.printStackTrace();
+				}
+				if (eliminado) {
+					System.out.println("Día eliminado con éxito");
+				} else {
+					System.out.println("No se pudo eliminar ese día");
+				}
+			}
+			case 5 -> {
+				try {
+					System.out.println(Datos.mesMayorGasto());
+				} catch (IOException e) {
+					System.err.println("No pudo hacerse la consulta");
+					e.printStackTrace();
+				}
+			}
+			case 6 -> {
+				System.out.println("Mes a consultar");
+				String mes = scan.nextLine();
+				try {
+					String result = Datos.listadoGastosMes(mes);
+					if (result != null) {
+						System.out.println(result);
+					} else {
+						System.out.println("No se pudo hacer la consulta");
+					}
+				} catch (IOException e) {
+					System.err.println("No pudo hacerse la consulta");
+					e.printStackTrace();
+				}
+			}
 			case 0 -> {
 				System.out.println("Cerrando programa...");
 			}
@@ -62,7 +118,7 @@ public class Main {
 				System.err.println("Opción no válida");
 			}
 			}
-		} while(op != 0);
+		} while (op != 0);
 		scan.close();
 
 	}

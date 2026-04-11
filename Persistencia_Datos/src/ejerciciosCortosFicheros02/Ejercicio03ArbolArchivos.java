95e07020a9f57b8a6651d91deb0ddb620573405c
Yisus <jga547@educa.madrid.org>
2026-04-11 20:31:37 +0200

    añadido comentario

--- Persistencia_Datos/src/ejerciciosCortosFicheros02/Ejercicio03ArbolArchivos.java
+++ Persistencia_Datos/src/ejerciciosCortosFicheros02/Ejercicio03ArbolArchivos.java
@@ -3,8 +3,10 @@ package ejerciciosCortosFicheros02;
 import java.io.File;
 
 public class Ejercicio03ArbolArchivos {
-	
-	public static void arbolArchivos(File directorio, int nivel) {	
+
+	public static void arbolArchivos(File directorio, int nivel) { // nivel sólo tiene la función de introducir
+																	// tabulación para representar visualmente los
+																	// distintos niveles jerárquicos
 		if (!directorio.isDirectory()) {
 			System.err.println("El fichero pasado no es un directorio");
 			return;
